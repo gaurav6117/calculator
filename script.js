0896df8a4693ace9f6bccac3108bfd6e040b772a
Gaurav6117 <gaurav.chouhan@neosoftmail.com>
2021-09-21 17:28:02 +0530

    console.log removed

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-        var flag = false; 
-      function clearHistory() {
-        console.log("clear called");
-        $("#ulist").empty();
-      }
-      let screen = document.getElementById("screen");
-      buttons = document.querySelectorAll("button");
-      let screenValue = "";
-      for (item of buttons) {
-        item.addEventListener("click", (e) => {
-          buttonText = e.target.innerText;
-          console.log("Button text is ", buttonText);
+function clearHistory() {
+  $("#ulist").empty();
+}
+let screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
 
-          if (buttonText == "C") {
-            screenValue = "";
-            screen.value = screenValue;
-          } 
-          else if (buttonText == "=") {
-            if (eval(screenValue)) {
-              let elem = document.createElement("li");
-              elem.innerHTML = `${screen.value} => ${eval(screenValue)}`;
-              document.getElementById("ulist").appendChild(elem);
-            }
-            screen.value = eval(screenValue);
-          } 
-          else {
-            screenValue += buttonText;
-            screen.value = screenValue;
-          }
-        });
+    if (buttonText == "C") {
+      screenValue = "";
+      screen.value = screenValue;
+    }
+    else if (buttonText == "=") {
+      if (eval(screenValue)) {
+        let elem = document.createElement("li");
+        elem.innerHTML = `${screen.value} = ${eval(screenValue)}`;
+        document.getElementById("ulist").appendChild(elem);
       }
-   
+      screen.value = eval(screenValue);
+    }
+    else {
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }
+  });
+}
+
